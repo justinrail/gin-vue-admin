d25d6c31d573f794c8c002369fdcd63c6cd02fff
abc <you@example.com>
2022-06-09 16:53:37 +0800

    update

---
+++ web/src/api/hub_gatewayDeviceTree.js
@@ -0,0 +1,18 @@
+import service from '@/utils/request'
+
+
+// @Tags Gateway Device
+// @Summary 获取gatewaydevice列表
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data query request.PageInfo true "获取gatewaydevice列表"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
+// @Router /zone/getZoneList [get]
+export const getGatewayDeviceTree = (params) => {
+  return service({
+    url: '/gatewaydevicetree/getgatewaydevicetreebody',
+    method: 'get',
+    params
+  })
+}
